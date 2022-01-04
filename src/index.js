517c3b3820958e2c409a7715d80d73bce8c7adaa
Rostisla8 <86406897+Rostisla8@users.noreply.github.com>
2022-01-04 13:13:49 +0300

    docs: write code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let strings = n.toString().split('');
+    let newarr2 = [];
+    if(strings[0] == '-'){
+        strings.shift();
+    }
+    console.log(strings)
+    let newarr = strings.reverse().join('');
+    return newarr/1;
 }
